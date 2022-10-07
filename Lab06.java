a59bd261666225567f1a5f1a6f9d3aa1b243ef55
atamezmunoz <78508833+atamezmunoz@users.noreply.github.com>
2022-10-07 10:49:37 -0700

    Created Lab06 Template

---
+++ Lab06.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Lab06 {
+
+    // TODO : remove TODOs when you're done
+
+    /**
+     * TODO : function description
+     *
+     * @param nums TODO : param description
+     * @param k TODO : param description
+     */
+    public static void rearrange(List<Integer> nums, int k) {
+        // TODO : Given a list of integer nums , rearrange the list by shifting elements to the right up to k steps. k is guaranteed to be positive.
+    }
+
+    public static void main(String[] args){
+        // TODO : test
+    }
+
+}
